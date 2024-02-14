3154ee12197f02ae2796abcc0da9f38b6a322e34
Omnicrafter <chen.jeffrey.a@gmail.com>
2024-02-13 23:17:07 -0600

    completed useAxios hook for card and pokemon

--- src/hooks.js
+++ src/hooks.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import axios from "axios";
+import { v1 as uuid } from "uuid";
 
 function useFlip() {
   const [isFaceUp, setIsFaceUp] = useState(true);
@@ -9,4 +11,16 @@ function useFlip() {
   return [isFaceUp, flipCard];
 }
 
+function useAxios(baseUrl) {
+  const [cards, setCards] = useState([]);
+
+  const addCard = async (urlModifier = "") => {
+    console.log(baseUrl + urlModifier);
+    const response = await axios.get(baseUrl + urlModifier);
+    setCards((cards) => [...cards, { ...response.data, id: uuid() }]);
+  };
+  return [cards, addCard];
+}
 export default useFlip;
+
+export { useAxios };
